557f8a794e8cebaa1e57458b26b2d6b80afe2492
Jisang Hong <wona23@gmail.com>
2021-05-06 14:25:09 +0900

    Add code for create account and password hashing

--- users/users.mutations.js
+++ users/users.mutations.js
@@ -1,3 +1,4 @@
+import bcrypt from 'bcrypt';
 import client from '../client';
 
 export default {
@@ -18,9 +19,17 @@ export default {
           ],
         },
       });
-
-      // hash password
-      // save and return the user
+      console.log(existingUser);
+      const uglyPassword = await bcrypt.hash(password, 10);
+      return client.user.create({
+        data: {
+          username,
+          email,
+          firstName,
+          lastName,
+          password: uglyPassword,
+        },
+      });
     },
   },
 };
